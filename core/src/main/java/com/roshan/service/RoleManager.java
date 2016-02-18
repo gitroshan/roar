f0b8e6532a5b9b44dcf10d7928b7344e954f39cd
roshan.p <roshan.p@aeturnum.com>
2016-02-18 16:25:13 +0530

    first commit with all the project files

---
+++ core/src/main/java/com/roshan/service/RoleManager.java
@@ -0,0 +1,33 @@
+package com.roshan.service;
+
+import com.roshan.model.Role;
+
+import java.util.List;
+
+/**
+ * Business Service Interface to handle communication between web and
+ * persistence layer.
+ *
+ * @author <a href="mailto:dan@getrolling.com">Dan Kibler </a>
+ */
+public interface RoleManager extends GenericManager<Role, Long> {
+    /**
+     * {@inheritDoc}
+     */
+    List getRoles(Role role);
+
+    /**
+     * {@inheritDoc}
+     */
+    Role getRole(String rolename);
+
+    /**
+     * {@inheritDoc}
+     */
+    Role saveRole(Role role);
+
+    /**
+     * {@inheritDoc}
+     */
+    void removeRole(String rolename);
+}
