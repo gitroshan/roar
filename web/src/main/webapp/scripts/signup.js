f22bd65ff13ebb9e6747997eeb26e9ac22cf14c6
roshan.p <roshan.p@aeturnum.com>
2016-03-09 15:36:19 +0530

    progress overlay

--- web/src/main/webapp/scripts/signup.js
+++ web/src/main/webapp/scripts/signup.js
@@ -2,22 +2,16 @@
 
 function validateSignUpForm(form) {
 	
-	showProgressOverlay('register-box-body', '<fmt:message key="loading.sign.in"/>');
+	showProgressOverlay('register-box-body', '<fmt:message key="loading.sign.up"/>');
 	    
 	if($("#signUpError") != null){
     	$("#signUpError").hide();
     }
-	var valid = validateSignup(form, $("#signUpErrorJs"));
-	//var valid = (errorMessage == null || errorMessage == '');
 	
-//	if (!valid) {
-//		if($("#signUpErrorJs") != null) {
-//			$("#signUpErrorJs").show();
-//			hideProgressOverlay('register-box-body');
-//	    }
-//     }
-	 hideProgressOverlay('register-box-body');
-     return valid;
+	var valid = validateSignup(form);
+
+	hideProgressOverlay('register-box-body');
+    return valid;
 }
 
 </script>
